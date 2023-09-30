afc9b39c40e64875400f1753ee22f288e23f8f49
litaoli <lli205@illinois.edu>
2023-09-30 11:08:03 -0500

    Initial Commit

---
+++ ReGithubDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReGithubDemo
+//
+//  Created by Litao Li on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReGithubDemo/ReGithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReGithubDemoApp.swift
+//  ReGithubDemo
+//
+//  Created by Litao Li on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ReGithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
